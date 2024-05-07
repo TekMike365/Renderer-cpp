0a9a277b02543c58d4af347b9fcc3f7551c0b0c9
TekMike365 <miskosklenka@gmail.com>
2024-05-07 17:01:39 +0200

    Added Render() function (broken)

--- Renderer/src/Renderer.cpp
+++ Renderer/src/Renderer.cpp
@@ -8,6 +8,7 @@ namespace Renderer {
     Pixel* screen = nullptr;
     int screenWidth = 0;
     int screenHeight = 0;
+    int camera = NONE;
 
     int Init(int width, int height)
     {
@@ -16,6 +17,13 @@ namespace Renderer {
         screen = new Pixel[width * height];
         if (screen == nullptr)
             return 1;
+        
+        for (int i = 0; i < screenWidth * screenHeight; i++) {
+            screen[i].r = 0x00;
+            screen[i].g = 0x00;
+            screen[i].b = 0x00;
+            screen[i].d = -1;
+        }
 
         return 0;
     }
@@ -23,6 +31,146 @@ namespace Renderer {
     int Cleanup()
     {
         delete[] screen;
+        return 0;
+    }
+
+    int SetCamera(int cameraType)
+    {
+        if (cameraType != ORTHOGRAPHIC_CAMERA && cameraType != PERSPECTIVE_CAMERA) {
+            return 1;
+        }
+        camera = cameraType;
+        return 0;
+    }
+
+    Vec3 ProjectPoint(Vec3 point) {
+        switch (camera)
+        {
+            case ORTHOGRAPHIC_CAMERA:
+                return point;
+            case PERSPECTIVE_CAMERA:
+                // TODO project point correctly
+                return point;
+            default:
+                return point;
+        }
+    }
+
+    float GetTriangleArea(Vec3 p1, Vec3 p2, Vec3 p3)
+    {
+        Vec3 v1 = p1 - p3;
+        Vec3 v2 = p2 - p3;
+        return v1.Cross(v2).GetScale() / 2;
+    }
+
+    float Lerp(float v1, float v2, float t) 
+    {
+        return (1 - t) * v1 + t * v2;
+    }
+
+    float TLerp(float v1, float v2, float v3, Vec2 t) 
+    {
+        float i = Lerp(v1, v2, t.x);
+        return Lerp(v3, i, t.y);
+    }
+
+    Vec2 GetLerpValues(Vec2 point, Vec3 p1, Vec3 p2, Vec3 p3) {
+        Vec2 va = Vec2(p2.x, p2.y) - Vec2(p1.x, p1.y);
+        Vec2 vc = point - Vec2(p3.x, p3.y);
+
+        float b = vc.y * (p1.x - p3.x) - vc.x * (p1.y - p3.y);
+        float a = vc.y * va.x - vc.x * va.y;
+
+        if (a == 0)
+            return Vec2(-1, -1);
+
+        float t = - b / a;
+
+        Vec2 vi = Vec2(p1.x, p1.y) + va * t;
+        Vec2 vn = vi - Vec2(p3.x, p3.y);
+
+        if (vn.GetScale() == 0)
+            return Vec2(-1, -1);
+        
+        float s = vc.GetScale() / vn.GetScale();
+
+        return Vec2(t, s);
+    }
+
+    int Render(Vec3 *vertices, int *indices, int indexCount)
+    {
+        // iterate trough triplets
+        for (int i = 0; i < indexCount; i += 3) {
+            Vec3 points[3] = {
+                ProjectPoint(vertices[i]),
+                ProjectPoint(vertices[i + 1]),
+                ProjectPoint(vertices[i + 2])
+            };
+
+            // Convert to pixels
+            bool render = false;
+            for (int i = 0; i < 3; i++) {
+                points[i] *= screenWidth;
+
+                if (points[i].x > -screenWidth / 2 &&
+                    points[i].x < screenWidth / 2 &&
+                    points[i].y > -screenHeight / 2 &&
+                    points[i].y < screenHeight / 2 &&
+                    points[i].z >= 0
+                ) {
+                    render = true;
+                }
+            }
+            if (!render)
+                continue;
+
+
+            int p1_x = (int)std::min(points[0].x, std::min(points[1].x, points[2].x));
+            int p1_y = (int)std::min(points[0].y, std::min(points[1].y, points[2].y));
+            int p2_x = (int)std::max(points[0].x, std::max(points[1].x, points[2].x));
+            int p2_y = (int)std::max(points[0].y, std::max(points[1].y, points[2].y));
+            int width = p2_x - p1_x;
+            int height = p2_y - p1_y;
+
+            // Rasterize
+            for (int i = 0; i < height; i++) {
+                for (int j = 0; j < width; j++) {
+                    int x = p1_x + j;
+                    int y = p1_y + i;
+                    int idx = screenWidth * y + x;
+
+                    // is point in triangle?
+                    Vec3 pixelPos = Vec3(x, y, 0);
+                    float a  = GetTriangleArea(points[0], points[1], points[2]);
+                    float a1 = GetTriangleArea(pixelPos, points[0], points[1]);
+                    float a2 = GetTriangleArea(pixelPos, points[1], points[2]);
+                    float a3 = GetTriangleArea(pixelPos, points[2], points[0]);
+                    
+                    float tolerance = 1.0f;
+                    float diff = a1 + a2 + a3 - a;
+                    bool isInside = diff <= tolerance && diff >= -tolerance;
+
+                    if (!isInside)
+                        continue;
+
+                    // lerp Z coords
+                    Vec2 lerpVals = GetLerpValues(Vec2(x, y), points[0], points[1], points[2]);
+                    if (lerpVals.x < 0 || lerpVals.y < 0)
+                        continue;
+
+                    float depth = TLerp(points[0].z, points[1].z, points[2].z, lerpVals);
+                    Pixel pixel = screen[idx];
+                    if (depth > pixel.d && pixel.d != -1)
+                        continue;
+                    
+                    pixel.r = 0xff;
+                    pixel.g = 0x00;
+                    pixel.b = 0xff;
+
+                    screen[i] = pixel;
+                }
+            }
+        }
         return 0;
     }
 
--- Renderer/src/Renderer.h
+++ Renderer/src/Renderer.h
@@ -5,6 +5,10 @@
 #include "vector/Vec4.h"
 #include "vector/Mat4.h"
 
+#define NONE 0
+#define ORTHOGRAPHIC_CAMERA 1
+#define PERSPECTIVE_CAMERA 2
+
 namespace Renderer {
 
     struct Pixel {
@@ -16,6 +20,8 @@ namespace Renderer {
 
     int Init(int width, int height);
     int Cleanup();
+    int SetCamera(int cameraType);
+    int Render(Vec3* vertices, int* indices, int indexCount);
     int SaveBMP(const char* filepath);
 
 }
